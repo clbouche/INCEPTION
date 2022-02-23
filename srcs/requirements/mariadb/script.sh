89a92be555856e6309cf65be09d9562f7959399d
Clara BOUCHER <clbouche@e2r4p20.clusters.42paris.fr>
2022-02-23 11:05:19 +0100

    wordpress OK

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -4,8 +4,14 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
     until mysqladmin ping; do
         sleep 2
     done
-    mysql -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; FLUSH PRIVILEGES;"
-    mysql -e "GRANT ALL ON wordpress.* TO 'wp-admin'@'localhost' IDENTIFIED BY 'wp-admin' WITH GRANT OPTION; ALTER USER 'root'@'localhost'; FLUSH PRIVILEGES;"
+    mysql -u root -e "SET GLOBAL general_log=1;"
+    mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
+    mysql -u root -e "CREATE DATABASE ${DB_WORDPRESS};"
+    mysql -u root -e "CREATE USER 'admin'@'*' IDENTIFIED BY 'admin';"
+    mysql -u root -e "GRANT ALL ON *.* TO 'admin'@'%';"
+    mysql -u root -e "CREATE USER 'wp_user'@'%' IDENTIFIED BY 'wp_password';"
+    mysql -u root -e "GRANT ALL ON ${DB_WORDPRESS}.* TO 'wp_user'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
     killall mysqld
     echo "End first run"
 fi
